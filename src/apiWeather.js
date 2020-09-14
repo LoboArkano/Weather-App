32d442e9cd270017f2fe675e85c05b1410e70b75
LoboArkano <pipiolo_mananes@hotmail.com>
2020-09-14 12:56:28 -0500

    Save weather data into a hash map

--- src/apiWeather.js
+++ src/apiWeather.js
@@ -1,25 +1,36 @@
 const apiWeather = (() => {
+  const localTime = (time) => {
+    const date = new Date(time * 1000);
+    const timestr = date.toLocaleTimeString();
+
+    return timestr;
+  };
+
   const getData = async (city) => {
+    const weather = new Map();
+
     try {
       const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=efb1c5442d9f74c1c186780238e45f35`, { mode: 'cors' });
       const apiData = await response.json();
 
-      console.log(apiData.coord.lon);
-      console.log(apiData.coord.lat);
-      console.log(apiData.weather[0]);
-      console.log(apiData.main.temp);
-      console.log(apiData.main.temp_min);
-      console.log(apiData.main.temp_max);
-      console.log(apiData.main.pressure);
-      console.log(apiData.main.humidity);
-      console.log(apiData.wind.speed);
-      console.log(apiData.wind.deg);
-      console.log(apiData.sys.country);
-      console.log(apiData.sys.sunrise);
-      console.log(apiData.sys.sunset);
+      weather.set('lon', apiData.coord.lon);
+      weather.set('lat', apiData.coord.lat);
+      weather.set('image', apiData.weather[0]);
+      weather.set('temo', apiData.main.temp - 273.15);
+      weather.set('temp_min', apiData.main.temp_min - 273.15);
+      weather.set('temp_max', apiData.main.temp_max - 273.15);
+      weather.set('pressure', apiData.main.pressure);
+      weather.set('humidity', apiData.main.humidity);
+      weather.set('speed', apiData.wind.speed);
+      weather.set('deg', apiData.wind.deg);
+      weather.set('country', apiData.sys.country);
+      weather.set('sunrise', localTime(apiData.sys.sunrise));
+      weather.set('sunset', localTime(apiData.sys.sunset));
     } catch (error) {
       alert('error');
     }
+
+    return weather;
   };
 
   return { getData };
