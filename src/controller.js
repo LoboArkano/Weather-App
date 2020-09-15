2043cb3b1c0940af5759b5001fef2d91d804e6df
LoboArkano <pipiolo_mananes@hotmail.com>
2020-09-14 21:47:51 -0500

    Add event to search the of ohter cities

--- src/controller.js
+++ src/controller.js
@@ -1,9 +1,21 @@
 import apiWeather from './apiWeather';
 import userInterface from './interface';
 
+const searchEvent = () => {
+  const searchBtn = document.getElementById('search');
+
+  searchBtn.addEventListener('click', async (e) => {
+    e.preventDefault();
+
+    const input = document.getElementById('input-city');
+
+    userInterface.render(await apiWeather.getData(input.value));
+  });
+};
 
 const getWeather = async () => {
   userInterface.loadUI();
+  searchEvent();
   userInterface.render(await apiWeather.getData('guadalajara'));
 };
 
