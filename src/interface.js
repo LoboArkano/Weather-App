05eb0de6cbcf98d6b5cf4da1ee7b1e493d80f372
LoboArkano <pipiolo_mananes@hotmail.com>
2020-09-16 12:34:57 -0500

    Build html structure for weather info

--- src/interface.js
+++ src/interface.js
@@ -11,10 +11,122 @@ const userInterface = (() => {
     return form;
   };
 
-  const loadUI = (dataWeather) => {
+  const cityInfo = () => {
+    const cityInfo = document.createElement('div');
+
+    cityInfo.setAttribute('id', 'cityInfo');
+
+    return cityInfo;
+  };
+
+  const weatherImg = () => {
+    const imageCont = document.createElement('div');
+    const weatherImg = document.createElement('img');
+    const imageDesc = document.createElement('p');
+
+    weatherImg.setAttribute('id', 'weather-img');
+    imageDesc.setAttribute('id', 'img-desc');
+
+    imageCont.append(weatherImg, imageDesc);
+
+    return imageCont;
+  };
+
+  const tempCont = () => {
+    const tempCont = document.createElement('div');
+    const temp = document.createElement('p');
+    const tempMax = document.createElement('p');
+    const tempMin = document.createElement('p');
+
+    temp.setAttribute('id', 'temp');
+    tempMax.setAttribute('id', 'temp-max');
+    tempMin.setAttribute('id', 'temp-min');
+
+    tempCont.append(temp, tempMax, tempMin);
+
+    return tempCont;
+  };
+
+  const windCont = () => {
+    const windCont = document.createElement('div');
+    const speed = document.createElement('p');
+    const deg = document.createElement('p');
+
+    speed.setAttribute('id', 'speed');
+    deg.setAttribute('id', 'deg');
+
+    windCont.append(speed, deg);
+
+    return windCont;
+  };
+
+  const atmCont = () => {
+    const atmCont = document.createElement('div');
+    const pressure = document.createElement('p');
+    const humidity = document.createElement('p');
+
+    pressure.setAttribute('id', 'pressure');
+    humidity.setAttribute('id', 'humidity');
+
+    atmCont.append(pressure, humidity);
+
+    return atmCont;
+  };
+
+  const sunCont = () => {
+    const sunCont = document.createElement('div');
+    const sunrise = document.createElement('p');
+    const sunset = document.createElement('p');
+
+    sunrise.setAttribute('id', 'sunrise');
+    sunset.setAttribute('id', 'sunset');
+
+    sunCont.append(sunrise, sunset);
+
+    return sunCont;
+  };
+
+  const locationCont = () => {
+    const location = document.createElement('div');
+    const lon = document.createElement('p');
+    const lat = document.createElement('p');
+
+    lon.setAttribute('id', 'lon');
+    lat.setAttribute('id', 'lat');
+
+    location.append(lon, lat);
+
+    return location;
+  };
+
+  const weatherInfo = () => {
+    const weatherInfo = document.createElement('div');
+
+    weatherInfo.append(
+      cityInfo(),
+      weatherImg(),
+      tempCont(),
+      windCont(),
+      atmCont(),
+      sunCont(),
+      locationCont(),
+    );
+
+    return weatherInfo;
+  };
+
+  const weatherCont = () => {
+    const weatherCont = document.createElement('main');
+
+    weatherCont.append(weatherInfo());
+
+    return weatherCont;
+  };
+
+  const loadUI = () => {
     const content = document.getElementById('content');
 
-    content.append(searchForm());
+    content.append(searchForm(), weatherCont());
   };
 
   return { loadUI };
